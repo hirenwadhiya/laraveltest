c5cacc2b1309217612e731a918b73f0e933cd543
Hiren Wadhiya <hirenwadhiya@gmail.com>
2024-11-23 13:32:03 +0530

    correct api response

--- app/Http/Controllers/UserAuthController.php
+++ app/Http/Controllers/UserAuthController.php
@@ -6,8 +6,8 @@ use App\Http\Requests\UserRegistrationRequest;
 use App\Http\Resources\UserRegistrationResource;
 use App\Models\User;
 use Exception;
-use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use function App\Helpers\failedResponse;
 use function App\Helpers\successResponse;
 
@@ -18,7 +18,7 @@ class UserAuthController extends Controller
         try {
             $userData = $request->safe()->only(['name', 'email', 'password']);
             $user = User::query()->create($userData);
-            return successResponse(__('message.user.register.success'), new UserRegistrationResource($user));
+            return successResponse(__('message.user.register.success'), new UserRegistrationResource($user), Response::HTTP_CREATED);
         } catch (Exception $exception) {
             return failedResponse(__('message.user.register.failed'));
         }
