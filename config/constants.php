51713304dbd8e13a0e766af09e15fae36d4f3ea2
Hiren Wadhiya <hirenwadhiya@gmail.com>
2024-11-24 22:49:35 +0530

    limit published date criteria

--- config/constants.php
+++ config/constants.php
@@ -4,7 +4,7 @@ return [
     'api_urls'   =>  [
         'news_api' => 'https://newsapi.org/v2/everything?apiKey=%s&q=%s&sortBy=publishedAt',
         'guardian_api' => 'https://content.guardianapis.com/search?api-key=%s&q=%s',
-        'newyork_times_api' => 'https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=%s&q=%s',
+        'newyork_times_api' => 'https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=%s&q=%s&begin_date=20120101',
     ],
     'api_keys'   =>  [
         'news_api' => env('NEWSAPI_API_KEY', ''),
